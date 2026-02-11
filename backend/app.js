26bad7e00e61019b49757ffcdbcafddbd3b160fd
dheeraj0274 <136884023+dheeraj0274@users.noreply.github.com>
2026-02-11 10:05:07 +0530

    render-proxy

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 import configurePassport from './utils/passport.js';
 import cors from 'cors'
 const app = express();
+app.set("trust proxy", 1);
 
 
 app.use(express.json());
